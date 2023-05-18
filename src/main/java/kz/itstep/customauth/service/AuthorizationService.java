fa19591c942b76893c123ec49912f8efe68cdff3
diasospanov <diaskakdela@gmail.com>
2023-05-18 19:42:45 +0600

    main functional

---
+++ src/main/java/kz/itstep/customauth/service/AuthorizationService.java
@@ -0,0 +1,15 @@
+package kz.itstep.customauth.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService {
+    public String generateToken(String login){
+
+        return null;
+    }
+
+    private String shepherLogin(String login){
+        return null;
+    }
+}
